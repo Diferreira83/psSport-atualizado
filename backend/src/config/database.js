2d035c492337e1183ff6d07ee81ab8410e791764
Divanildo Ferreira dos Santos <divanildo.santos83@gmail.com>
2026-01-18 09:14:46 -0300

    ajustes

--- backend/src/config/database.js
+++ backend/src/config/database.js
@@ -17,5 +17,17 @@ console.log({
 
 module.exports = pool;
 
+/*const mysql = require('mysql2');
+
+const connection = mysql.createPool({
+  host: process.env.DB_HOST,
+  user: process.env.DB_USER,
+  password: process.env.DB_PASSWORD,
+  database: process.env.DB_NAME,
+  waitForConnections: true,
+  connectionLimit: 10
+});
+
+module.exports = connection;*/
 
 
